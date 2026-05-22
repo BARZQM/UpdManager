8d930f2f916bb928bb513ffc7217dcce6c322f8c
_micon <miha1901.sovgav@gmail.com>
2026-05-22 11:57:47 +0300

    Update version.lua

--- version.lua
+++ version.lua
@@ -1,25 +1,34 @@
 local UpdateManager = {}
 
-UpdateManager.LatestBuildCode = 501 -- Your actual max version
-UpdateManager.BuildName = "5.1"
-UpdateManager.BuildVersion = "test"
 
--- The logic is now safely hidden on GitHub
+UpdateManager.LatestBuildCode = 518
+UpdateManager.BuildName = "5.1"
+UpdateManager.BuildVersion = "release"
+
 function UpdateManager.runSystemCheck(currentVersionCode: number)
 	if type(currentVersionCode) ~= "number" then
 		return "Error: Local build version code must be a number."
 	end
 	
 	if currentVersionCode < UpdateManager.LatestBuildCode then 
+		-- Local version is outdated
 		return string.format(
-			"Update available! Current: %s. Latest: %s (%s).",
+			"Update available! Your build: %s. Latest official stable build: %s (%s). Please update.",
 			tostring(currentVersionCode),
 			UpdateManager.BuildName, 
 			UpdateManager.BuildVersion
 		)
-	end
+	elseif currentVersionCode > UpdateManager.LatestBuildCode then
 	
-	return "Success: Admin panel is up to date."
+		return string.format(
+			"Warning: You are running an unreleased preview/test build (%s). Stable version is %s.",
+			tostring(currentVersionCode),
+			tostring(UpdateManager.LatestBuildCode)
+		)
+	else
+
+		return "Success: Admin panel is perfectly up to date."
+	end
 end
 
 return UpdateManager
